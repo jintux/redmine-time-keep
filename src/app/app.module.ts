b8433e8f9f817504c8fa1b312317426ead9b27c3
Johan 't Hart <johanthart@gmail.com>
2017-10-16 23:07:21 +0200

    Add beginnings of material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatSelectModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule, MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
